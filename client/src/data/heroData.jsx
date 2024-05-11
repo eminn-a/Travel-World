cae466c65a7939bdbb92af46211e4178d8d67d65
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-05-11 19:03:44 +0300

    Added some hero fixes

--- client/src/data/heroData.jsx
+++ client/src/data/heroData.jsx
@@ -1,23 +1,21 @@
 export const data = {
   home: {
     img: "https://images.pexels.com/photos/386009/pexels-photo-386009.jpeg",
-    title: "Your journey your story!",
+    title: "Your journey, your story!",
     text: "Chose your favorite destination!",
     url: "/",
     btnClass: "show",
   },
   about: {
     img: "https://images.pexels.com/photos/1313814/pexels-photo-1313814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    title: "Hero title",
-    text: "Hero text",
+    title: "About Us",
+    text: "About Us",
     url: "/",
   },
   contacts: {
     img: "https://images.pexels.com/photos/1851481/pexels-photo-1851481.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    title: "Hero title",
-    text: "Hero text",
+    title: "Contact Us",
+    text: "Contact Us",
     url: "/",
   },
 };
-
-let a = "../";
