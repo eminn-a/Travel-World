fd9e484dce36acaefc4f87bdc2d2101c747a440c
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-05-11 22:54:04 +0300

    Hero css fixes

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,16 +1,16 @@
 import styles from "./HeroStyles.module.css";
 
-const Hero = ({ image, title, text, url, name }) => {
+const Hero = ({ image, title, text, url, btnName, img }) => {
   return (
     <div className={styles.hero}>
-      <img src={image} alt="Hero img" />
+      <img src={img} alt="Hero img" />
       <div className={styles.heroText}>
         <h1>{title}</h1>
         <ul></ul>
         <p>{text}</p>
-        {name && (
+        {btnName && (
           <a href={url} className={styles.show}>
-            {name}
+            {btnName}
           </a>
         )}
       </div>
