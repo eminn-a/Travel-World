579f9abf822896e97f4bfbd565e6635c2a5a0993
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-05-15 20:37:13 +0300

    Footer added

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import styles from "./FooterStyles.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.top}>
+        <div>
+          <h1>Trawel-World</h1>
+          <p>You'r unforgetable memories!</p>
+        </div>
+        <div className={styles.icons}>
+          <i class="fa-brands fa-square-facebook"></i>
+          <i class="fa-brands fa-square-instagram"></i>
+          <i class="fa-brands fa-square-youtube"></i>
+          <i class="fa-brands fa-square-twitter"></i>
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <div>
+          <h4>First Section</h4>
+          <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third!</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Second Section</h4>
+          <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third!</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Third Section</h4>
+          <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third!</li>
+          </ul>
+        </div>
+      </div>
+      <h5 className={styles.rights}>
+        All rights reserved © 2024 Emin Apturaim
+      </h5>
+    </div>
+  );
+};
+
+export default Footer;
