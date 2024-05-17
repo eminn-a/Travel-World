b3dfb69994d4dc44f5f5ee18512201df449a25d1
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-05-17 14:17:47 +0300

    About page added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,4 @@
+import AboutUs from "../components/AboutUs/AboutUs";
 import Hero from "../components/Hero/Hero";
 import { heroData } from "../data/heroData";
 
@@ -5,6 +6,7 @@ const About = () => {
   return (
     <>
       <Hero {...heroData.about} />
+      <AboutUs />
     </>
   );
 };
