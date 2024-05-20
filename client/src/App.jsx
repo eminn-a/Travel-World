0b15703bde59c012b5748f4bd68378305dde8557
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-05-20 09:54:52 +0300

    React routerd added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import { Routes, Route } from "react-router-dom";
+
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import AboutPage from "./pages/AboutPage";
@@ -9,10 +11,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <HomePage />
-      <CatalogPage />
-      <AboutPage />
-      <ContactsPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+      </Routes>
       <Footer />
     </>
   );
