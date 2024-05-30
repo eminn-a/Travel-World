9266437cf4d185989b58972003b4a171ef457eff
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-05-30 14:02:17 +0300

    Homepage refactoring

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
+import { useEffect, useState } from "react";
+
+import * as destinationService from "./../services/destinationServices";
+
 import Hero from "../components/Hero/Hero";
 import Destination from "../components/Destination/Destination";
 import Blogs from "../components/Blogs/Blogs";
 
 import { heroData } from "../data/heroData";
-// import { destinationData } from "../data/destinationData";
 import { tripData } from "../data/tripData";
-import hero from "../assets/hero.jpg";
-import { useEffect, useState } from "react";
-import * as destinationService from "./../services/destinationServices";
 
 const HomePage = () => {
   const [destinations, setDestinations] = useState([]);
@@ -15,10 +15,9 @@ const HomePage = () => {
     destinationService.getAll().then((res) => setDestinations(res));
   }, []);
 
-  // console.log(destinations);
   return (
     <>
-      <Hero {...heroData.home} image={hero} />
+      <Hero {...heroData.home} />
       <Destination data={destinations} />
       <Blogs data={tripData} />
     </>
