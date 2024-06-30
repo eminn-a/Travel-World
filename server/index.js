a923c3a9cc34b8a142c0eadd500d237ea4a3b6b5
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-06-30 17:12:16 +0300

    test

--- server/index.js
+++ server/index.js
@@ -22,4 +22,33 @@ mongoose
 // app.listen(3030, () =>
 //   console.log("RESTful server is listening on port 3030...")
 // );
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}@trawel-world.gnfgv12.mongodb.net/?appName=Trawel-World`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 module.exports = app;
