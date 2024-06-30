fad0d7465ab0de145bc823beb0e0294e9ec00686
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-06-30 18:34:26 +0300

    test

--- server/api/index.js
+++ server/api/index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const routes = require("./routes");
-const { auth } = require("./middlewares/authMiddleware");
+const routes = require("../routes"); // Adjust path as necessary
+const { auth } = require("../middlewares/authMiddleware"); // Adjust path as necessary
 
 const app = express();
 
@@ -14,21 +14,20 @@ app.use(auth);
 app.use(routes);
 
 const uri = process.env.MONGO_URI;
+
 mongoose
   .connect(uri)
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
-// Export the app to be used as a serverless function
+// Export the app as a serverless function handler
 module.exports = (req, res) => {
-  // Set up Vercel's serverless function handler for the Express app
   return new Promise((resolve, reject) => {
-    // Attach the Express app to the serverless function handler
-    const server = app.listen(() => {
-      resolve(server);
-    });
+    // Create an instance of the Express app for handling requests
+    const server = app(req, res);
+    resolve(server);
     server.on("error", (err) => {
       reject(err);
     });
-  })(req, res);
+  });
 };
