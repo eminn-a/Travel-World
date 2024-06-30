86aac0b8f74587591cfb56022a67a412fd95bec7
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-06-30 17:51:28 +0300

    test

--- server/index.js
+++ server/index.js
@@ -2,42 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const databaseUser = process.env.DB_USER;
-const uri = `mongodb+srv://${databaseUser}@trawel-world.gnfgv12.mongodb.net/?retryWrites=true&w=majority&appName=Trawel-World`;
-
-const routes = require("./routes.js");
-const { auth } = require("./middlewares/authMiddleware.js");
+const routes = require("./routes");
+const { auth } = require("./middlewares/authMiddleware");
 
 const app = express();
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-mongoose
-  .connect(`mongodb+srv://${databaseUser}@trawel-world.gnfgv12.mongodb.net/`)
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.log(err));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -45,4 +13,12 @@ app.use(cors());
 app.use(auth);
 app.use(routes);
 
-app.listen(5050, () => console.log("Server is listening at port 5050..."));
+const uri = process.env.MONGO_URI;
+mongoose
+  .connect(uri)
+  .then(() => console.log("DB connecteed"))
+  .catch((err) => console.log(err));
+
+app.listen(3030, () =>
+  console.log("RESTful server is listening on port 3030...")
+);
