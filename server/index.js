d0d535063eac4bf4240aa59c6b0cbc4d96d0eae1
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-06-30 13:33:14 +0300

    test

--- server/index.js
+++ server/index.js
@@ -6,17 +6,29 @@ const { auth } = require("./middlewares/authMiddleware");
 
 const app = express();
 
+// Middleware setup
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(auth);
 
+// Route setup
 app.use(routes);
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/destinations")
-  .then(() => console.log("DB connecteed"))
-  .catch((err) => console.log(err));
-app.listen(3030, () =>
-  console.log("RESTful server is listening on port 3030...")
-);
+// Database connection (consider moving this to a separate file and initializing it on demand)
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/destinations");
+    console.log("DB connected");
+  } catch (err) {
+    console.log("DB connection error", err);
+  }
+};
+
+// Initialize database connection
+connectToDatabase();
+
+// Export the Express app as a handler
+module.exports = (req, res) => {
+  app(req, res);
+};
