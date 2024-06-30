ec49741dc9870bd3f41d8264eab6b0eecdd78d23
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-06-30 15:24:29 +0300

    test

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 const { auth } = require("./middlewares/authMiddleware");
+const encodeURIComponent = require("querystring").escape;
 
 const app = express();
 
@@ -13,20 +14,22 @@ app.use(cors());
 app.use(auth);
 app.use(routes);
 
+const user = encodeURIComponent(process.env.DB_USER);
+const pass = encodeURIComponent(process.env.DB_PASS);
+const host = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+
 // mongoose
-//   .connect("mongodb://127.0.0.1:27017/destinations")
-//   .then(() => console.log("DB connecteed"))
+//   .connect(`mongodb://${user}:${pass}@${host}/${dbName}`)
+//   .then(() => console.log("DB connected"))
 //   .catch((err) => console.log(err));
 
 app.listen(3030, () =>
   console.log("RESTful server is listening on port 3030...")
 );
 
-const user = process.env.DB_USER;
-console.log(user);
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${user}@trawel-world.gnfgv12.mongodb.net/?retryWrites=true&w=majority&appName=Trawel-World`;
+const uri = `mongodb+srv://${user}:${pass}@${host}/?retryWrites=true&w=majority&appName=${dbName}`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -35,9 +38,10 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
