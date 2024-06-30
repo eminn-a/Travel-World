e8ea5c12181c629fec841fd8f5549ec7c9e9bbc8
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-06-30 16:08:48 +0300

    test

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ const destinationsController = require("./controllers/destinationsController");
 const blogController = require("./controllers/blogController");
 
 router.use("/users", userController);
-router.use("/data/destinations", destinationsController);
-router.use("/data/blogs", blogController);
+router.use("/destinations", destinationsController);
+router.use("/blogs", blogController);
 
 module.exports = router;
