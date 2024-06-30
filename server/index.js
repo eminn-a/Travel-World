cbfe066814349bd405b029dd4da8edaecca06322
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-06-30 15:20:11 +0300

    test

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -6,53 +7,46 @@ const { auth } = require("./middlewares/authMiddleware");
 
 const app = express();
 
-// Middleware setup
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(auth);
 app.use(routes);
 
-// Database connection
-mongoose
-  .connect(
-    "mongodb+srv://eminkataa93:5aieL1C8sYm3uzbt@trawel-world.vpg6eil.mongodb.net/?appName=trawel-world",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.error("DB connection error:", err));
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/destinations")
+//   .then(() => console.log("DB connecteed"))
+//   .catch((err) => console.log(err));
 
-// Export Express app as a function for Vercel
-module.exports = (req, res) => {
-  app(req, res);
-};
+app.listen(3030, () =>
+  console.log("RESTful server is listening on port 3030...")
+);
 
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const uri =
-//   "mongodb+srv://eminkataa93:5aieL1C8sYm3uzbt@trawel-world.vpg6eil.mongodb.net/?appName=trawel-world";
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+const user = process.env.DB_USER;
+console.log(user);
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${user}@trawel-world.gnfgv12.mongodb.net/?retryWrites=true&w=majority&appName=Trawel-World`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
