85021d536b9fb9ba9579a7def4be7c4028dd325d
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-08-27 18:38:36 +0300

    Modal outer click close

--- client/src/components/AuthModal/AuthModal.jsx
+++ client/src/components/AuthModal/AuthModal.jsx
@@ -1,176 +1,3 @@
-// import { useState } from "react";
-// import { useForm } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
-// import { loginValidation } from "../../validations/loginValidation";
-// import styles from "./AuthModal.module.css";
-// import { login, create } from "../../services/authServices";
-// import { setUserData } from "../../utils/utils";
-// import toast from "react-hot-toast";
-// import { useMutation } from "@tanstack/react-query";
-
-// const AuthModal = ({ show, closeModal, setUser }) => {
-//   const [registered, setRegistered] = useState(false);
-
-//   const validationSchema = loginValidation(registered);
-
-//   const {
-//     register,
-//     handleSubmit,
-//     reset,
-//     formState: { errors, isSubmitting },
-//   } = useForm({
-//     resolver: yupResolver(validationSchema),
-//   });
-
-//   const loginUserMutation = useMutation({
-//     mutationFn: (data) => login(data.email, data.password),
-//     onSuccess: (user) => {
-//       if (user) {
-//         setUserData(user);
-//         setUser({ ...user });
-//         closeModal();
-//         reset();
-//       }
-//       setRegistered(false);
-//       toast.success(`Hello, ${user.email}`);
-//     },
-//   });
-
-//   const createUserMutation = useMutation({
-//     mutationFn: (data) => create(data.email, data.password),
-//     onSuccess: (user) => {
-//       if (user) {
-//         setUserData(user);
-//         setUser({ ...user });
-//         closeModal();
-//         reset();
-//       }
-//       setRegistered(false);
-//       toast.success(`Hello, ${user.email}`);
-//     },
-//   });
-
-//   const onSubmit = async (data) => {
-//     if (!registered) {
-//       loginUserMutation.mutate(data);
-//     } else {
-//       createUserMutation.mutate(data);
-//     }
-//   };
-
-//   return (
-//     <>
-//       {show ? (
-//         <div className={styles.modalContainer}>
-//           <div className={styles.wrapper}>
-//             <div className={styles.closeBtn}>
-//               <button
-//                 onClick={() => {
-//                   closeModal();
-//                   reset();
-//                   setRegistered(false);
-//                 }}
-//               >
-//                 <i className="fa-solid fa-xmark"></i>
-//               </button>
-//             </div>
-//             <div className={styles.titleText}>
-//               {registered ? (
-//                 <div className={`${styles.title} ${styles.signup}`}>
-//                   Register now
-//                 </div>
-//               ) : (
-//                 <div className={`${styles.title} ${styles.login}`}>
-//                   Sign in{" "}
-//                 </div>
-//               )}
-//             </div>
-//             <div className={styles.formContainer}>
-//               <div className={styles.formInner}>
-//                 <form
-//                   className={styles.login}
-//                   onSubmit={handleSubmit(onSubmit)}
-//                 >
-//                   <div className={styles.field}>
-//                     <input
-//                       {...register("email")}
-//                       type="text"
-//                       placeholder="Email Address"
-//                     />
-//                   </div>
-//                   <div className={styles.field}>
-//                     <input
-//                       {...register("password")}
-//                       type="password"
-//                       placeholder="Password"
-//                     />
-//                   </div>
-//                   {registered ? (
-//                     <div className={styles.field}>
-//                       <input
-//                         {...register("rePassword")}
-//                         type="password"
-//                         placeholder="Re-Password"
-//                       />
-//                     </div>
-//                   ) : (
-//                     <div className={styles.passLink}>
-//                       <a href="#">Forgot password?</a>
-//                     </div>
-//                   )}
-//                   <div className={`${styles.field} ${styles.btn}`}>
-//                     <div className={styles.btnLayer}></div>
-//                     <input
-//                       type="submit"
-//                       value={registered ? "Register" : "Sign In"}
-//                     />
-//                   </div>
-//                 </form>
-//                 {errors && (
-//                   <p className="errorMsg">
-//                     {errors[Object.keys(errors)[0]]?.message}
-//                   </p>
-//                 )}
-//                 {!registered ? (
-//                   <div className={styles.signupLink}>
-//                     <hr />
-//                     Not a member?
-//                     <p
-//                       onClick={() => {
-//                         setRegistered(true);
-//                         reset();
-//                       }}
-//                     >
-//                       Register now
-//                     </p>
-//                     <hr />
-//                   </div>
-//                 ) : (
-//                   <div className={styles.signupLink}>
-//                     <hr />
-//                     Already registered?
-//                     <p
-//                       onClick={() => {
-//                         setRegistered(false);
-//                         reset();
-//                       }}
-//                     >
-//                       Sign in now
-//                     </p>
-//                     <hr />
-//                   </div>
-//                 )}
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       ) : null}
-//     </>
-//   );
-// };
-
-// export default AuthModal;
-
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -232,12 +59,22 @@ const AuthModal = ({ show, closeModal, setUser }) => {
       createUserMutation.mutate(data);
     }
   };
+  const handleOuterClick = () => {
+    closeModal();
+    reset();
+    setShowPassword(false);
+    setShowRePassword(false);
+  };
+
+  const handleInnerClick = (e) => {
+    e.stopPropagation();
+  };
 
   return (
-    <>
+    <div onClick={handleOuterClick}>
       {show ? (
         <div className={styles.modalContainer}>
-          <div className={styles.wrapper}>
+          <div onClick={handleInnerClick} className={styles.wrapper}>
             <div className={styles.closeBtn}>
               <button
                 onClick={() => {
@@ -357,7 +194,7 @@ const AuthModal = ({ show, closeModal, setUser }) => {
           </div>
         </div>
       ) : null}
-    </>
+    </div>
   );
 };
 
