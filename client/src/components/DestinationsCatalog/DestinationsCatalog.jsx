9cffc95fef860001955f69221789646773b3994a
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-10-26 22:38:16 +0300

    Destination catalog changes(removed title and desc)

--- client/src/components/DestinationsCatalog/DestinationsCatalog.jsx
+++ client/src/components/DestinationsCatalog/DestinationsCatalog.jsx
@@ -6,11 +6,11 @@ import SingleDestinationCatalogItem from "./SingleDestinationCatalogItem";
 const DestinationsCatalog = ({ data, error, isLoading }) => {
   return (
     <div className={styles.trip}>
-      <h1>Catalog items here</h1>
+      {/* <h1>Catalog items here</h1>
       <p>
         Discover unforgettable adventures and amazing memories we've made
         together!
-      </p>
+      </p> */}
       {/* <DestinationFilter /> */}
 
       {isLoading && <Spinner></Spinner>}
