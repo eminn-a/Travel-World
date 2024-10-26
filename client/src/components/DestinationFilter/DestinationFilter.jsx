82404ee3d1ed923d8dafe8d52a9ebb153f7911f1
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-10-26 22:37:53 +0300

    Destination search input added

--- client/src/components/DestinationFilter/DestinationFilter.jsx
+++ client/src/components/DestinationFilter/DestinationFilter.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
+import styles from "./DestinationFilter.module.css";
 
 export default function DestinationFilter() {
   const [search, setSearch] = useState("");
-  console.log(search);
   return (
-    <div>
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search..."
-      />
+    <div className={styles.searchContainer}>
+      <div className={styles.inputWrapper}>
+        <i className={`fa-solid fa-magnifying-glass ${styles.searchIcon}`}></i>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search..."
+        />
+      </div>
     </div>
   );
 }
