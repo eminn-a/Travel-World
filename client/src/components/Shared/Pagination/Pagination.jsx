fb4d1fffd82dc5ff69d2a82a5c49ab4ffd79e2a3
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-10-28 20:23:40 +0200

    pagination design added

--- client/src/components/Shared/Pagination/Pagination.jsx
+++ client/src/components/Shared/Pagination/Pagination.jsx
@@ -1,5 +1,20 @@
 import styles from "./PaginationStyles.module.css";
 
 export default function Pagination() {
-  return <h1>Pagination</h1>;
+  return (
+    <>
+      <div className={styles.pagination}>
+        <a href="#">
+          <i class="fa-solid fa-chevron-left"></i>
+        </a>
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">
+          <i class="fa-solid fa-chevron-right"></i>
+        </a>
+      </div>
+    </>
+  );
 }
