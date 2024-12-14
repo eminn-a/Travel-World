0502be299c8a0f716d58916a4ec24b35b6266aed
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-12-14 22:43:06 +0200

    test

--- server/index.js
+++ server/index.js
@@ -10,11 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://travel-world-c12.web.app",
-  })
-);
+app.use(cors());
 app.use(auth);
 app.use(routes);
 
